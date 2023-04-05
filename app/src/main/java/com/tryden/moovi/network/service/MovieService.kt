4de839ef67f0d093681b6e74049132b9f16f1117
Tyler Ryden <tryden11@gmail.com>
2023-04-05 14:32:21 -0400

    Added client interceptor to retrofit builder for api key

--- app/src/main/java/com/tryden/moovi/network/service/MovieService.kt
+++ app/src/main/java/com/tryden/moovi/network/service/MovieService.kt
@@ -6,7 +6,8 @@ import retrofit2.http.GET
 
 interface MovieService {
 
-    // Now playing todo: remove api key
-    @GET("movie/now_playing?api_key=7bfe007798875393b05c5aa1ba26323e")
+    // Now playing
+    @GET("movie/now_playing")
     suspend fun getMoviesNowPlaying() : Response<MovieNowPlayingResponse>
+
 }
